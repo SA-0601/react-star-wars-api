53bd006eab897f0b1b260683c14398c6ae1bdd55
shubha agrawal <shubh.cap@gmail.com>
2023-03-29 17:45:04 -0400

    showing details

--- src/components/StarshipCard.js
+++ src/components/StarshipCard.js
@@ -1,12 +1,29 @@
 import Card from "react-bootstrap/Card";
+import { useState } from "react";
 
 function StarShipCard(props) {
+  const [showDetails, setShowDetails] = useState(false);
+
+  function toggleDetails() {
+    setShowDetails(!showDetails);
+  }
+
   return (
     <div>
-      <Card className="bg-success border-dark" style={{width: "20rem", height: "10rem"}}>
-      <Card.Body className="text-center">
-        <Card.Title className='text-white'>{props.name}</Card.Title>
-      </Card.Body>
+      <Card
+        className="bg-success border-dark"
+        style={{ width: "20rem", height: "10rem" }}
+      >
+        <Card.Body onClick={toggleDetails}>
+          <Card.Title className="text-white">{props.name} </Card.Title>
+          {showDetails && (
+            <ul className="mt-1 text-white">
+              <li>Model: {props.model}</li>
+              <li>Manufacturer: {props.manufacturer}</li>
+              <li>Starship Class: {props.starship_class}</li>
+            </ul>
+          )}
+        </Card.Body>
       </Card>
     </div>
   );
